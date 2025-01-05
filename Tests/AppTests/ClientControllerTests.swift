1f2a0df2086c8baf94b3c50382aaaea358a7cc82
Grant <grant@grantcohoe.com>
2025-01-05 15:35:58 -0500

    actually working tests

--- Tests/AppTests/ClientControllerTests.swift
+++ Tests/AppTests/ClientControllerTests.swift
@@ -1,27 +1,49 @@
 @testable import swiftarr
 import XCTVapor
+import Testing
 
-final class ClientTests: XCTestCase {
-    var app: Application!
-
-    override func setUp() async throws {
-        try await super.setUp()
-        app = try await Application.testable()
-        try await app.asyncBoot()
-        try await app.startup()
-    }
-
-    override func tearDown() async throws {
+struct ClientControllerTests {
+    private func withApp(_ test: (Application) async throws -> ()) async throws {
+        let app = try await Application.testable()
+        do {
+            // try await app.autoMigrate()   
+            try await test(app)
+        }
+        catch {
+            // try? await app.autoRevert()
+            try await app.asyncShutdown()
+            throw error
+        }
         try await app.asyncShutdown()
-        try await super.tearDown()
     }
+    // var app: Application!
+
+    // override func setUp() async throws {
+    //     try await super.setUp()
+    //     app = try await Application.testable()
+    //     try await app.asyncBoot()
+    //     try await app.startup()
+    // }
+
+    // override func tearDown() async throws {
+    //     try await app.asyncShutdown()
+    //     try await super.tearDown()
+    // }
+	@Test("testClientHealthRoute")
     func testClientHealthRoute() async throws {
-        let res = try await app.client.get("http://localhost:8081/api/v3/client/health")
-        XCTAssertEqual(res.status, .ok)
+        try await withApp { app in
+            try await app.test(.GET, "/api/v3/client/health", afterResponse: { res async throws in 
+				XCTAssertEqual(res.status, .ok)
+			})
+        }
     }
 
-    // func testNonexistantRoute() async throws {
-    //     let res = try await app.client.get("http://localhost:8081/api/v3/nonexistant")
-    //     XCTAssertEqual(res.status, .notFound)
-    // }
+	@Test("testNonexistantRoute")
+    func testNonexistantRoute() async throws {
+		try await withApp { app in
+			try await app.test(.GET, "/api/v3/nonexistant", afterResponse: { res async throws in 
+				XCTAssertEqual(res.status, .notFound)
+			})
+		}
+    }
 }
